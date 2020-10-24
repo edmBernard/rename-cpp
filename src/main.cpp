fc46b81e68566859027912ff8797c8e8706d5b27
edmbernard <bernard.erwan@gmail.com>
2020-10-24 17:21:18 +0200

    feat(rename): add regex + update help

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <filesystem>
+#include <regex>
 
 #include <cxxopts.hpp>
 
@@ -15,13 +16,16 @@ int main(int argc, char **argv) {
     options.positional_help("[optional args]").show_positional_help();
 
     // clang-format off
-    options.add_options()("help", "Print help")
-      ("n,no-act", "show changes without doing anythings", cxxopts::value<bool>()->default_value("False"))
-      ("d,directory", "directory", cxxopts::value<std::string>(), "DIRECTORY")
-      ("r,recursive", "find file recursively", cxxopts::value<bool>())
+    options.add_options()
+      ("h,help", "Print help")
+      ("n,no-act", "show what files would have been renamed but do nothing")
+      ("d,directory", "the directory containing files to rename", cxxopts::value<std::string>(), "DIRECTORY")
+      ("regex", "the regular expression that will be matched against the filename", cxxopts::value<std::string>())
+      ("format", "the regex replacement format string", cxxopts::value<std::string>())
+      ("v,verbose", "print names of files successfully renamed")
       ;
     // clang-format on
-    options.parse_positional({"directory"});
+    options.parse_positional({"match", "replace", "directory"});
     auto result = options.parse(argc, argv);
 
     if (result.count("help")) {
@@ -43,13 +47,28 @@ int main(int argc, char **argv) {
       std::cout << "Error: specified directory is not a directory" << std::endl;
       return 1;
     }
+
+    const std::regex rule(result["match"].as<std::string>());
+    const std::string format(result["replace"].as<std::string>());
+
     for(auto& p: fs::directory_iterator(directory)) {
-      fs::path path_absolute = fs::absolute(p.path());
-      if (!fs::is_regular_file(path_absolute)) {
+      fs::path pathAbsolute = fs::absolute(p.path());
+      if (!fs::is_regular_file(pathAbsolute)) {
         continue;
       }
-      std::cout << path_absolute << std::endl;
-      std::cout << path_absolute.filename() << std::endl;
+
+      const std::string newFilename = std::regex_replace(pathAbsolute.filename().string(), rule, format);
+      const fs::path newPath = pathAbsolute.parent_path() / newFilename;
+
+      if (pathAbsolute != newPath) {
+        if (result.count("verbose")) {
+          std::cout << pathAbsolute.filename() << "  will be renamed in  " << newPath.filename() << std::endl;
+        }
+
+        if (!result.count("no-act")) {
+          fs::rename(pathAbsolute, newPath);
+        }
+      }
     }
 
 
